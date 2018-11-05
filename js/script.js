37acbc7784f4be04169d06ded2bd67e7c7c425fd
doman89 <mateusz.domanski.1989@gmail.com>
2018-11-05 23:53:45 +0100

    add class Task, and modify all function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,61 @@
+class Task {
+    constructor(textContent, endTime = 0) {
+        let _liElement = null; //new
+        let _textContent = textContent;
+        let _endTime = endTime;
+        let _isDone = false;
+        let _index = null;
+        this.getLiElement = () => _liElement; //new
+        this.setLiElement = li => _liElement = li;
+        this.getTextContent = () => _textContent;
+        this.getEndTime = () => _endTime;
+        this.getStatus = () => _isDone;
+        this.changeStatus = () => _isDone = !_isDone;
+        this.getIndex = () => _index;
+        this.setIndex = index => _index = index;
+    }
+    createListElement(removeCallbackFn) {
+        const li = document.createElement('li');
+        li.className = 'task';
+        li.textContent = `${this.getTextContent()} `;
+        const button = document.createElement('button');
+        button.textContent = 'Remove';
+        li.appendChild(button);
+        li.querySelector('button').addEventListener('click', removeCallbackFn);
+        li.dataset.index = null;
+        return this.setLiElement(li);
+    }
+    setDataset(index) {
+        const li = this.getLiElement();
+        li.dataset.index = this.setIndex(index);
+        return this.setLiElement(li);
+    }
+    toCompleted(restoreCallbackFn, deleteCallbackFn) {
+        const li = this.getLiElement();
+        li.innerHTML += ' ';
+        li.appendChild(document.createElement('button'));
+        let button = li.querySelector('button');
+        button.textContent = 'Restore';
+        button.addEventListener('click', restoreCallbackFn);
+        button = li.querySelector('button:nth-last-child(1)');
+        button.textContent = 'Delete';
+        button.addEventListener('click', deleteCallbackFn);
+        this.changeStatus();
+        return this.setLiElement(li);
+    }
+    toTodo(removeCallbackFn, restoreCallbackFn) {
+        const li = this.getLiElement();
+        let button = li.querySelector('button');
+        button.textContent = 'Remove';
+        button.removeEventListener('click', restoreCallbackFn);
+        button.addEventListener('click', removeCallbackFn);
+        button = li.querySelector('button:nth-last-child(1)');
+        button.remove();
+        this.changeStatus();
+        return this.setLiElement(li);
+    }
+}
+
 const form = document.querySelector('section form');
 const ulTaskList = document.querySelector('section .lists .todo ul');
 const ulCompletedList = document.querySelector('section .lists .done ul');
@@ -5,87 +63,85 @@ const input = document.querySelector('section input');
 const numberOfToDo = document.querySelector('section .lists .todo h2 span');
 const numberOfDone = document.querySelector('section .lists .done h2 span');
 
-const todoList = [];
-const doneList = [];
+const todoTasks = [];
 
-const deleteTask = e => {
-    const index = e.target.parentNode.dataset.index;
-    doneList.splice(index, 1);
-    ulCompletedList.textContent = null;
-    updateCompletedList();
-}
+const addTask = () => {
+    event.preventDefault();
+    const newItem = input.value.trim();
+    if (!newItem)
+        return;
+    const task = new Task(newItem);
+    task.createListElement(removeTask);
+    todoTasks.push(task);
+    updateTaskList();
+    input.value = '';
+};
 
 const updateTaskList = () => {
     ulTaskList.textContent = null;
-    todoList.forEach((task, index) => {
-        task.dataset.index = index;
-        ulTaskList.appendChild(task);
-    })
-    numberOfToDo.textContent = todoList.length;
+    numberOfToDo.textContent = todoTasks.filter((task, index) => {
+        if (!task.getStatus()) {
+            task.setDataset(index);
+            ulTaskList.appendChild(task.getLiElement());
+            return true;
+        }
+    }).length;
 }
 
 const updateCompletedList = () => {
     ulCompletedList.textContent = null;
-    doneList.forEach((task, index) => {
-        task.dataset.index = index;
-        ulCompletedList.appendChild(task);
-    })
-    numberOfDone.textContent = doneList.length;
+    numberOfDone.textContent = todoTasks.filter((task, index) => {
+        if (task.getStatus()) {
+            task.setDataset(index);
+            ulCompletedList.appendChild(task.getLiElement());
+            return true;
+        }
+    }).length;
     input.value = null;
 }
 
-const recoveryTask = e => {
+const deleteTask = e => {
     const index = e.target.parentNode.dataset.index;
-    const button = doneList[index].querySelector('button');
-    button.textContent = 'Remove';
-    button.removeEventListener('click', recoveryTask);
-    button.addEventListener('click', removeTask);
-    const deleteButton = doneList[index].querySelector('button:nth-last-child(1)');
-    deleteButton.remove();
-    todoList.push(...doneList.splice(index, 1));
+    todoTasks.splice(index, 1);
+    ulCompletedList.textContent = null;
+    updateCompletedList();
+}
+
+const restoreTask = e => {
+    const index = e.target.parentNode.dataset.index;
+    todoTasks[index].toTodo(removeTask, restoreTask);
     updateCompletedList();
     updateTaskList();
 }
 
 const removeTask = e => {
+    console.log(e.target);
     const index = e.target.parentNode.dataset.index; //get index of element
-    todoList[index].innerHTML += ' '; // add space between old button and new to create
-    todoList[index].appendChild(document.createElement('button')); //create new button
-    const button = todoList[index].querySelector('button'); //way to first button
-    button.textContent = 'Restore';
-    button.addEventListener('click', recoveryTask);
-    const deleteButton = todoList[index].querySelector('button:nth-last-child(1)'); //way to new button
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', deleteTask);
-    doneList.push(...todoList.splice(index, 1)); //transfer task to completed 
+    todoTasks[index].toCompleted(restoreTask, deleteTask);
     updateCompletedList();
     updateTaskList();
 };
 
-const addTask = () => {
-    event.preventDefault();
-    const newTask = input.value.trim();
-    if (!newTask)
-        return;
-    const tempTask = document.createElement('li');
-    tempTask.className = 'task';
-    tempTask.textContent = newTask;
-    tempTask.innerHTML += ' <button>Remove</button>';
-    todoList.push(tempTask);
-    tempTask.querySelector('button').addEventListener('click', removeTask);
-    updateTaskList();
-    input.value = '';
-};
+
 
 const filterTasks = e => {
-    const tempTodoList = todoList.filter(task => task.textContent.toLowerCase().includes(e.target.value.toLowerCase()));
-    const tempDoneList = doneList.filter(task => task.textContent.toLowerCase().includes(e.target.value.toLowerCase()));
+    const tempTodoList = todoClass.filter(task => task.hasText(e.target.value));
+    let taskNumberToDo = 0;
+    let taskNumberCompleted = 0;
     ulCompletedList.textContent = ulTaskList.textContent = null;
-    tempTodoList.forEach(li => ulTaskList.appendChild(li));
-    tempDoneList.forEach(li => ulCompletedList.appendChild(li));
-    numberOfToDo.textContent = tempTodoList.length;
-    numberOfDone.textContent = tempDoneList.length;
+    tempTodoList.forEach(task => {
+        if (task.getStatus()) {
+            ulCompletedList.appendChild(task.createLiElement());
+            taskNumberCompleted++;
+        } else {
+            ulTaskList.appendChild(task.createLiElement());
+            taskNumberToDo++;
+        }
+
+    })
+    numberOfToDo.textContent = taskNumberToDo;
+    numberOfDone.textContent = taskNumberCompleted;
 }
 
 form.addEventListener('submit', addTask);
-input.addEventListener('input', filterTasks);
+// input.addEventListener('input', filterTasks);
